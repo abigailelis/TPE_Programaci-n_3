c213ca56622442dd0ba2ae1b5ea6c11a60ae87d7
Abigail Samanta Elis <abigailelis.s@hotmail.com>
2024-09-24 15:14:44 -0300

    Crea la clase Tarea

---
+++ Tarea.java
@@ -0,0 +1,12 @@
+public class Tarea {
+
+    private String id;
+    private String nombre;
+    private Integer tiempo;
+    private Boolean ctricia;
+    private Integer prioridad;
+
+    public Tarea(){
+        
+    }
+}
