fe9e63a32c55220672f39e0bf7d264dac18ee48c
Abigail Samanta Elis <86386981+abigailelis@users.noreply.github.com>
2024-10-04 10:48:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+package ProgramacionIII.tpe;
+
 public class Main {
 
-	public static void main(String args[]) {
-		Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
-	
+	public static void main(String[] args) {
+		//Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
+
+		Servicios servicios = new Servicios("./tpe/datasets/Procesadores.csv", "./tpe/datasets/Tareas.csv");
+
+		System.out.println(servicios.servicio1("T1"));
+		System.out.println(servicios.servicio2(true));
+		System.out.print(servicios.servicio3(35,60));
 	}
 }
